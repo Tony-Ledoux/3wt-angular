0f1487bf55f0afafa0ed2b2d0da25b9766b942ad
Tony Ledoux <Tony.Ledoux@gmail.com>
2026-06-13 22:39:33 +0200

    expirydate

--- frontend/src/app/features/main/components/add-inventory-item/add-inventory-item.ts
+++ frontend/src/app/features/main/components/add-inventory-item/add-inventory-item.ts
@@ -3,7 +3,7 @@ import { Component, computed, effect, inject, input, signal } from '@angular/cor
 import { toSignal } from '@angular/core/rxjs-interop';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { storageDevice } from '@app/core/types/device';
-import { ProductCategory } from '@app/core/types/productCategories';
+import { ProductCategory, StorageRule } from '@app/core/types/productCategories';
 import { ProductDto } from '@app/core/types/products';
 import { ButtonComponent } from '@app/shared/components/button/button';
 import { LabeledInput } from '@app/shared/components/form/labeled-input/labeled-input';
@@ -29,15 +29,67 @@ export class AddInventoryItem {
   categories = computed<ProductCategory[]>(() => this.data().categories)
   devices = computed<storageDevice[]>(() => this.data().devices)
   defaultUnit = computed<string>(() => this.product()?.defaultUnit ?? '');
-  storageAdvice = signal<string | null>(null);
   storageLocationId = toSignal(
     this.inventoryForm.controls.StorageLocationId.valueChanges.pipe(
       startWith(this.inventoryForm.controls.StorageLocationId.value)
     )
   );
-  
- 
-  
+  selectedDevice = computed<storageDevice | null | undefined>(() => {
+    let id: null | string | number | undefined = this.storageLocationId();
+    if (!id) return null;
+    id = parseInt(id, 10);
+    return this.devices().find(x => x.id === id);
+  });
+
+  filteredRules = computed<StorageRule[]>(() => {
+    const device = this.selectedDevice();
+    if (!device) return [];
+    const relevantRules: StorageRule[] = [];
+    for (const cat of this.categories()) {
+      const ruleMatch = cat.storageRules.filter(rule => rule.deviceType === device.deviceType)
+      if (ruleMatch.length > 0) {
+        relevantRules.push(...ruleMatch)
+      }
+    }
+
+    return relevantRules
+  });
+
+  multiplier = computed<number>(() => {
+    const rules = this.filteredRules();
+    if (rules.length === 0) return 0;
+    // return the sum of all rule.mulipier
+    const totaal = rules.reduce((accumulator, rule) => accumulator + rule.multiplier, 0);
+    return totaal;
+  })
+  storageAdvice = computed<null | string>(() => {
+    if (this.multiplier() === 0 || this.multiplier() > 0) return null;
+    return `We adviseren om ${this.product().productName} niet op te slaan in een ${this.selectedDevice()?.deviceType}`;
+  });
+  expiryDate = computed<string>(() => {
+    const multiplier = this.multiplier();
+    const shelfLife = this.product().shelfLifeClosedMinutes;
+    if (!shelfLife) return '';
+    let shelfLifeDays = shelfLife / 1440
+    if(multiplier !== 0){
+      shelfLifeDays = shelfLifeDays * multiplier;
+    }
+    const expiryDate = new Date();
+    expiryDate.setDate(expiryDate.getDate());
+    expiryDate.setHours(0, 0, 0, 0);// Reset time portion for accuracy
+
+    // Add the calculated days to the date
+    expiryDate.setDate(expiryDate.getDate() + Math.round(shelfLifeDays));
+
+    // Update the form field with a standard YYYY-MM-DD string
+    const dateString = expiryDate.toISOString().split('T')[0];
+    return dateString;
+  })
+
+
+
+
+
   public get deviceOptionsList(): SelectOptions[] {
     return this.devices().map(x => ({ label: `${x.name} - ${x.deviceType}`, value: x.id })).sort((a, b) => a.label.localeCompare(b.label));
   }
@@ -45,18 +97,18 @@ export class AddInventoryItem {
   constructor() {
     effect(() => {
       const currentData = this.data();
+      const exp = this.expiryDate();
       if (!currentData.product) return
 
       this.inventoryForm.get('Unit')?.patchValue(this.defaultUnit(), { emitEvent: false });
-      //const multiplier = currentData.categories[0].storagerules.filter(x=>x.)
-
-      // if storageLocationIdChanges
-      //if multiplier is negative, set warning else calculate exirationdate if product.shelflifeClosed is known
+      if(exp >= new Date().toISOString()){
+        this.inventoryForm.get('ExpiryDate')?.patchValue(this.expiryDate(),{emitEvent:false});
+      }else {
+        this.inventoryForm.get('ExpiryDate')?.patchValue('',{emitEvent:false});
+      }
     });
   }
-  onExpiryDateChange(event: string) {
-    console.log(event);
-  }
+
 
   onSubmit() {
     console.log(this.inventoryForm.value);
